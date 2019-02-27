5478c61751bfd7ad480f03e97586072317118752
PeterH <kungfupanda2016@163.com>
2019-02-27 20:59:34 +0800

    添加读取小型数据集的文件

---
+++ read_utils_5000.py
@@ -0,0 +1,45 @@
+def label2onehot(element):
+    if element == '1':
+        return [1,0,0,0,0]
+    elif element == '2':
+        return [0,1,0,0,0]
+    elif element == '3':
+        return [0,0,1,0,0]
+    elif element == '4':
+        return [0,0,0,1,0]
+    elif element == '5':
+        return [0,0,0,0,1]
+
+def Return_train():
+  file = open("ECG5000_TRAIN")
+  lines = file.readlines()
+  alldata = []
+  for line in lines:
+    line = line.split(',')
+    label = line[0]
+    label = label2onehot(label)
+    train = line[1:]
+    train = [float(i) for i in train]
+    seqlen = len(train)
+    alldata.append((train,label,seqlen))
+    #sys.stdout.flush()
+  #for i in alldata:
+  #    print(i[0])
+  return alldata
+
+def Return_test():
+  file = open("ECG5000_TEST")
+  lines = file.readlines()
+  alldata = []
+  for line in lines:
+    line = line.split(',')
+    label = line[0]
+    label = label2onehot(label)
+    train = line[1:]
+    train = [float(i) for i in train]
+    seqlen = len(train)
+    alldata.append((train,label,seqlen))
+    #sys.stdout.flush()
+  #for i in alldata:
+  #    print(i[0])
+  return alldata
